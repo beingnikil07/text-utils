445686709bcc4eacc668361258fa3e14f2bdea91
Nikhil Kumar Rana <109665731+beingnikhil07@users.noreply.github.com>
2022-12-15 16:03:48 +0530

    Adding favicons

--- src/App.js
+++ src/App.js
@@ -3,22 +3,28 @@ import "./App.css";
 import NavBar from "./Components/NavBar";
 import TextForm from "./Components/TextForm";
 function App() {
-  const [mode,setMode]=useState("light");
-  const toggleMode=()=>{
-    if(mode==='light')
-    {
+  const [mode, setMode] = useState("light");
+  const toggleMode = () => {
+    if (mode === "light") {
       setMode("dark");
-      document.body.style.backgroundColor="grey";
-    }else{
+      document.body.style.backgroundColor = "grey";
+      document.title = "TextUtils-dark mode";
+    } else {
       setMode("light");
-      document.body.style.backgroundColor="white";
+      document.body.style.backgroundColor = "white";
+      document.title = "TextUtils-Light mode";
     }
-  }
+  };
   return (
     <div>
-      <NavBar title="TextUtils" about="About Textutils" mode={mode} toggleMode={toggleMode} />
+      <NavBar
+        title="TextUtils"
+        about="About Textutils"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
       <div className="container my-4">
-        <TextForm heading="Enter the text to analyze " mode={mode}/> 
+        <TextForm heading="Enter the text to analyze " mode={mode} />
       </div>
     </div>
   );
