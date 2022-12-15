b829810ea2a8568fb3fa9b58ae38e361bdc3f8ae
Nikhil Kumar Rana <109665731+beingnikhil07@users.noreply.github.com>
2022-12-15 12:16:45 +0530

    adding more functionalities to textUtils

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -11,12 +11,25 @@ export default function TextForm(props) {
     let newText = Text.toLowerCase();
     setText(newText);
   };
+  const handleClearClick = () => {
+    let newText = "";
+    setText(newText);
+  };
+  const handleCopyClick = () => {
+    let newText = document.getElementById("mybox");
+    newText.select(); // selects the text
+    navigator.clipboard.writeText(newText.value);
+  };
+  const handleExtraSpaceClick = () => {
+    let newText = Text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
 
   const handleOnChange = (event) => {
     setText(event.target.value);
   };
 
-  const [Text, setText] = useState("");     //removing the default value of textarea
+  const [Text, setText] = useState(""); //removing the default value of textarea
 
   return (
     <>
@@ -36,6 +49,24 @@ export default function TextForm(props) {
           <button className="btn btn-primary my-3 mx-1" onClick={handleLoClick}>
             Convert to Lowercase
           </button>
+          <button
+            className="btn btn-primary my-3 mx-1"
+            onClick={handleClearClick}
+          >
+            Clear
+          </button>
+          <button
+            className="btn btn-primary my-3 mx-1"
+            onClick={handleCopyClick}
+          >
+            Copy
+          </button>
+          <button
+            className="btn btn-primary my-3 mx-1"
+            onClick={handleExtraSpaceClick}
+          >
+            Remove extra space
+          </button>
         </div>
       </div>
       <div className="container">
@@ -43,7 +74,8 @@ export default function TextForm(props) {
         <p>
           {Text.split(" ").length} words and {Text.length} characters
         </p>
-        <p>{0.008 * Text.split(" ").length} Minutes read</p>   {/* 0.008 is the time of reading one word in minutes */}
+        <p>{0.008 * Text.split(" ").length} Minutes read</p>{" "}
+        {/* 0.008 is the time of reading one word in minutes */}
         <h2>Preview</h2>
         <p>{Text}</p>
       </div>
