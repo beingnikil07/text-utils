9ac4f648e03d4dfdd4078b8bd175c27aba35ea7a
Nikhil Kumar Rana <109665731+beingnikhil07@users.noreply.github.com>
2022-12-16 09:34:57 +0530

    Final modification in textUtils

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,76 +1,40 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom';
 
-export default function NavBar(props) {
-  /* To use props in a functional based component we have to write props in function argv area */
-  return (
-    <div>
-      <nav
-        className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
-      >
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            {props.title}
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="/navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a
-                  className="nav-a "
-                  style={{
-                    textDecoration: "none",
-                    color: props.mode === "light" ? "black" : "white",
-                  }}
-                  aria-current="page"
-                  href="/"
-                >
-                  Home
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div
-          className={`form-check form-switch w-25 text-${
-            props.mode === "dark" ? "light" : "dark"
-          }`}
-          style={{ display: "flex", justifyContent: "flex-end" }}
-        >
-          <input
-            className="form-check-input"
-            type="checkbox"
-            onClick={props.toggleMode}
-            role="switch"
-            id="flexSwitchCheckDefault"
-          />
-          <label className="form-check-label " htmlFor="flexSwitchCheckDefault">
-            Dark Mode
-          </label>
-        </div>
-      </nav>
-    </div>
-  );
+export default function Navbar(props) {
+    return (
+        <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">{props.title}</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link" aria-current="page" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/about">{props.aboutText}</Link>
+                        </li>
+                    </ul>
+                    <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
+                        <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault"/>
+                        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable DarkMode</label>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
 }
 
-// Props can be an array,string,object etc
-NavBar.propTypes = {
-  // only string can be passed,This is how we can prevent  wrong props data
-  title: PropTypes.string.isRequired,
-  about: PropTypes.string.isRequired,
-};
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    aboutText: PropTypes.string.isRequired
+}
 
-NavBar.defaultProps = {
-  title: "set title here",
-  about: "set about here",
-};
+Navbar.defaultProps = {
+    title: 'Set title here',
+    aboutText: 'About'
+  };
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,96 +1,65 @@
-import React from "react";
-import { useState } from "react";
+import React, {useState} from 'react'
+
 
 export default function TextForm(props) {
-  const handleUpClick = () => {
-    let newText = Text.toUpperCase();
-    setText(newText);
-  };
+    const handleUpClick = ()=>{
+        let newText = text.toUpperCase();
+        setText(newText)
+        props.showAlert("Converted to uppercase!", "success");
+    }
 
-  const handleLoClick = () => {
-    let newText = Text.toLowerCase();
-    setText(newText);
-  };
-  const handleClearClick = () => {
-    let newText = "";
-    setText(newText);
-  };
-  const handleCopyClick = () => {
-    let newText = document.getElementById("mybox");
-    newText.select(); // selects the text
-    navigator.clipboard.writeText(newText.value);
-  };
-  const handleExtraSpaceClick = () => {
-    let newText = Text.split(/[ ]+/);
-    setText(newText.join(" "));
-  };
+    const handleLoClick = ()=>{ 
+        let newText = text.toLowerCase();
+        setText(newText)
+        props.showAlert("Converted to lowercase!", "success");
+    }
 
-  const handleOnChange = (event) => {
-    setText(event.target.value);
-  };
+    const handleClearClick = ()=>{ 
+        let newText = '';
+        setText(newText);
+        props.showAlert("Text Cleared!", "success");
+    }
 
-  const [Text, setText] = useState(""); //removing the default value of textarea
+    const handleOnChange = (event)=>{
+        setText(event.target.value) 
+    }
 
-  return (
-    <>
-      <div
-        className="container"
-        style={{ color: props.mode === "dark" ? "white" : "black" }}
-      >
-        <div className="mb-3">
-          <h1>{props.heading}</h1>
-          <textarea
-            className="form-control"
-            id="mybox"
-            onChange={handleOnChange}
-            value={Text}
-            rows="7"
-            style={{
-              backgroundColor: props.mode === "light" ? "white" :"#143153",
-              color: props.mode === "light" ? "black" : "whitesmoke",
-            }}
-          ></textarea>
-          <button className="btn btn-primary my-3 mx-1" onClick={handleUpClick}>
-            Convert to Uppercase
-          </button>
-          <button className="btn btn-primary my-3 mx-1" onClick={handleLoClick}>
-            Convert to Lowercase
-          </button>
-          <button
-            className="btn btn-primary my-3 mx-1"
-            onClick={handleClearClick}
-          >
-            Clear
-          </button>
-          <button
-            className="btn btn-primary my-3 mx-1"
-            onClick={handleCopyClick}
-          >
-            Copy
-          </button>
-          <button
-            className="btn btn-primary my-3 mx-1"
-            onClick={handleExtraSpaceClick}
-          >
-            Remove extra space
-          </button>
+    // Credits: A
+    const handleCopy = () => {
+        navigator.clipboard.writeText(text); 
+        props.showAlert("Copied to Clipboard!", "success");
+    }
+
+    // Credits: Coding Wala
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+        props.showAlert("Extra spaces removed!", "success");
+    }
+
+    const [text, setText] = useState(''); 
+    // text = "new text"; // Wrong way to change the state
+    // setText("new text"); // Correct way to change the state
+    return (
+        <>
+        <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}> 
+            <h1 className='mb-4'>{props.heading}</h1>
+            <div className="mb-3"> 
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#13466e':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
+            </div>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
         </div>
-      </div>
-      <div
-        className="container"
-        style={{ color: props.mode === "dark" ? "white" : "black" }}
-      >
-        <h2>Your text summary</h2>
-        <p>
-          {Text.split(" ").length} words and {Text.length} characters
-        </p>
-        <p>{0.008 * Text.split(" ").length} Minutes read</p>{" "}
-        {/* 0.008 is the time of reading one word in minutes */}
-        <h2>Preview</h2>
-        <p>
-          {Text.length>0?Text:"Enter something in the Above box to preview"}
-        </p>
-      </div>
-    </>
-  );
+        <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
+            <h2>Your text summary</h2>
+            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+            <p>{0.008 *  text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
+            <h2>Preview</h2>
+            <p>{text.length>0?text:"Nothing to preview!"}</p>
+        </div>
+        </>
+    )
 }
